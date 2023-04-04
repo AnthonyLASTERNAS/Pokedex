c85d5a1a4909a48b4f16e2679e47a24080f24e32
AnthonyLASTERNAS <lespike24@hotmail.fr>
2023-04-04 10:48:15 +0200

    passer des props

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard';
 
-function App() {
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
+const App = () => {
+
+
 
   return (
     <div className="App">
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
+
   )
 }
+
+
+
+
+
 
 export default App
